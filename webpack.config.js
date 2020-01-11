20150314bd4f42c5efb2c9ac0ecd206cc2d54290
sergiy <sergey.martynuk@gmail.com>
2020-01-11 17:38:11 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: "babel-loader"
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+};
