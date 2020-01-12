bc252dcf11fc369e632d2a345f83c3744ff8258d
sergiy <sergey.martynuk@gmail.com>
2020-01-12 13:59:45 +0200

    add arg mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,19 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                use: "babel-loader"
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin()
-    ]
+module.exports = (env={}, argv={}) => {
+    return {
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: "babel-loader"
+                },
+            ],
+        },
+        plugins: [
+            argv.mode === "development"
+                ? new HtmlWebpackPlugin()
+                : null
+        ].filter(plugin => !!plugin)
+    }
 };
